a569e0dadffc5f96faf46ee9d57d8fae93a2c459
Kyle Trantum <ktrantum@gmail.com>
2012-09-12 22:28:50 -0700

    wtf breaking stuff... added rubygems?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebsiteMaker::Application.config.secret_token = 'd66a4d58b5bc7b110d142ffcbd29829eb7cb57ef1cfe663fd44e87e5f54edf5857fd92a229e978342ca1186e473498e101206b174c54ff4cf90746a3ba6b5589'
