661d1563ec5e0cff1087563789547b59b5e07cc8
Kyle Trantum <ktrantum@gmail.com>
2012-10-01 12:53:45 -0700

    WebShare | Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebShare::Application.config.secret_token = '82c3632f52ce2cc04ff0da7bc073b8567b14973557a81e76645f97caf2306904a1e6bd370c3b07961333856f5f2907acdacb744fc511f2ce3f88ad42437da4de'
