9f1108a03a326973729ba7266416e5ac735b2d7f
Kyle Trantum <ktrantum@gmail.com>
2012-10-06 20:52:45 -0700

    Fixed sign_in to with remember token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,15 +9,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.authenticate(params[:session][:email],
-                             params[:session][:password])
-    if user.nil?
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user_path
+    else
       flash.now[:error] = "Invalid email or password."
       @title = "Sign in"
       render 'new'
-    else
-      sign_in user
-      redirect_to user_path
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121005062018) do
+ActiveRecord::Schema.define(:version => 20121007034631) do
 
   create_table "projects", :force => true do |t|
     t.string   "projectName"
@@ -39,8 +39,10 @@ ActiveRecord::Schema.define(:version => 20121005062018) do
     t.string   "salt"
     t.string   "encrypted_password"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
